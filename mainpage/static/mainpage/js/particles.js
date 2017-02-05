2924e085b1b5d4ed3ed53061d14192d849bea411
Jeffrey Witthuhn <jeffdwitt@gmail.com>
2017-02-05 15:41:14 -0600

    customized the page a bit, still needs custom css

---
+++ mainpage/static/mainpage/js/particles.js
@@ -0,0 +1,32 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#000000',
+    particleRadius:8,
+    proximity:100,
+    minRadiusPct:1, 
+    maxRadiusPct:2.2,
+    density:4000
+  });
+  //var intro = document.getElementById('intro');
+  //intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
