78b6f5dcba492ff7a23b5438c5b8f414cf20fc75
csciclub <csciclub@stcloudstate.edu>
2017-02-07 22:43:05 -0500

    removed parallax effect

--- mainpage/static/mainpage/js/particles.js
+++ mainpage/static/mainpage/js/particles.js
@@ -1,5 +1,5 @@
 /**
- * Particleground demo
+ * Particleground
  * @author Jonathan Nicol - @mrjnicol
  */
 
@@ -11,22 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
     proximity:100,
     minRadiusPct:1, 
     maxRadiusPct:2.2,
-    density:4000
+    density:2500,
+    parallax:false,
   });
   //var intro = document.getElementById('intro');
   //intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
 
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
